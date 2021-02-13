82352e2d2f272b190164f64eb79cbe28cff3a684
kunal <kunalmandot1999@gmail.com>
2021-02-13 18:37:19 +0530

    defined routes

--- src/routes/todos.js
+++ src/routes/todos.js
@@ -2,4 +2,32 @@ const express = require('express');
 
 const router = express.Router();
 
+router.route('/')
+  .get()
+  .post();
+
+router.route('/:todoId')
+  .get()
+  .put()
+  .delete();
+
+router.route('/:todoId/move-to-trash')
+  .put();
+
+router.route('/:todoId/restore')
+  .put();
+
+router.route('/:todoId/share')
+  .post();
+
+router.route('/:todoId/unshare')
+  .delete();
+
+router.route('/:todoId/tasks')
+  .post();
+
+router.route('/:todoId/tasks/:taskId')
+  .put()
+  .delete();
+
 module.exports = router;
