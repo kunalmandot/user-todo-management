dfdc1e0136f14484a04dde48a9a87089557742da
Kunal <kunalmandot1999@gmail.com>
2021-02-23 16:50:33 +0530

    removed duplications from joi schema

--- src/components/account/account.validation.js
+++ src/components/account/account.validation.js
@@ -1,6 +1,6 @@
 const Joi = require('joi');
 
-const signupSchema = Joi.object({
+const commonSchema = {
   email: Joi.string()
     .email()
     .required()
@@ -19,6 +19,11 @@ const signupSchema = Joi.object({
       'string.max': 'Password should contain at most 16 characters.',
       'any.required': 'Password should be present.',
     }),
+};
+
+const signupSchema = Joi.object({
+  email: commonSchema.email,
+  password: commonSchema.password,
   confirmPassword: Joi.string()
     .valid(Joi.ref('password'))
     .required()
@@ -29,47 +34,13 @@ const signupSchema = Joi.object({
 });
 
 const loginSchema = Joi.object({
-  email: Joi.string()
-    .email()
-    .required()
-    .messages({
-      'string.base': 'Email should be a type of string.',
-      'string.email': 'Email should be a valid email.',
-      'any.required': 'Email should be present.',
-    }),
-  password: Joi.string()
-    .min(8)
-    .max(16)
-    .required()
-    .messages({
-      'string.base': 'Password should be a type of string.',
-      'string.min': 'Password should contain at least 8 characters.',
-      'string.max': 'Password should contain at most 16 characters.',
-      'any.required': 'Password should be present.',
-    }),
+  email: commonSchema.email,
+  password: commonSchema.password,
 });
 
 const changePasswordSchema = Joi.object({
-  oldPassword: Joi.string()
-    .min(8)
-    .max(16)
-    .required()
-    .messages({
-      'string.base': 'Old password should be a type of string.',
-      'string.min': 'Old password should contain at least 8 characters.',
-      'string.max': 'Old password should contain at most 16 characters.',
-      'any.required': 'Old password should be present.',
-    }),
-  newPassword: Joi.string()
-    .min(8)
-    .max(16)
-    .required()
-    .messages({
-      'string.base': 'New password should be a type of string.',
-      'string.min': 'New password should contain at least 8 characters.',
-      'string.max': 'New password should contain at most 16 characters.',
-      'any.required': 'New password should be present.',
-    }),
+  oldPassword: commonSchema.password,
+  newPassword: commonSchema.password,
   confirmNewPassword: Joi.string()
     .valid(Joi.ref('newPassword'))
     .required()
